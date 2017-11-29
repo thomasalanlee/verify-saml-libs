6b45ded6a03ff2f32adab6b6a22e3fe99180c0c9
Tim Spence <timothywspence@gmail.com>
2017-11-29 16:55:35 +0000

    EID-297: fix typo
    Authors: @timwspence

--- src/test/java/uk/gov/ida/saml/core/test/builders/AuthnStatementBuilderTest.java
+++ src/test/java/uk/gov/ida/saml/core/test/builders/AuthnStatementBuilderTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AuthnStatementBuilderTest {
 
     @Test
-    public void shouldBuildWithCorretLOA() {
+    public void shouldBuildWithCorrectLOA() {
         AuthnStatement authnStatement = AuthnStatementBuilder.anEidasAuthnStatement().build();
 
         assertThat(authnStatement.getAuthnContext().getAuthnContextClassRef().getAuthnContextClassRef()).isEqualTo(EidasAuthnContext.EIDAS_LOA_SUBSTANTIAL);
