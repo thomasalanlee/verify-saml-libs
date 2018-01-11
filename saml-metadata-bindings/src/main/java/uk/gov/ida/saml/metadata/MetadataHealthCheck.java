993ebaac02492b0c4d6ce04e59a497b14870d11a
Hugh Emerson <hugh.emerson@digital.cabinet-office.gov.uk>
2018-01-11 14:09:44 +0000

    BAU: add metadata health check/refresh task classes from dropwizard-saml library
    Authors: @hugh-emerson, @michaelWalker

---
+++ saml-metadata-bindings/src/main/java/uk/gov/ida/saml/metadata/MetadataHealthCheck.java
@@ -0,0 +1,40 @@
+package uk.gov.ida.saml.metadata;
+
+import com.codahale.metrics.health.HealthCheck;
+import net.shibboleth.utilities.java.support.resolver.CriteriaSet;
+import org.opensaml.core.criterion.EntityIdCriterion;
+import org.opensaml.saml.metadata.resolver.MetadataResolver;
+import org.opensaml.saml.saml2.metadata.EntityDescriptor;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import static com.codahale.metrics.health.HealthCheck.Result.healthy;
+import static com.codahale.metrics.health.HealthCheck.Result.unhealthy;
+
+public class MetadataHealthCheck extends HealthCheck {
+
+    private MetadataResolver metadataResolver;
+    private String expectedEntityId;
+
+    @Inject
+    public MetadataHealthCheck(MetadataResolver metadataProvider,
+                               @Named("expectedEntityId") String expectedEntityId) {
+        this.metadataResolver = metadataProvider;
+        this.expectedEntityId = expectedEntityId;
+    }
+
+    public String getName() {
+        return "metadata";
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        CriteriaSet criteria = new CriteriaSet(new EntityIdCriterion(expectedEntityId));
+        EntityDescriptor entityDescriptor = metadataResolver.resolveSingle(criteria);
+        if (entityDescriptor != null) {
+            return healthy();
+        }
+        return unhealthy("Could not load the hub entity from the metadata provider");
+    }
+}
