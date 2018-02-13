acee6a49b7df9342d38a50f6d312ac66ec4d02df
IreneLau-GDS <irene.lau@digital.cabinet-office.gov.uk>
2018-02-13 16:25:50 +0000

    EID-344: EncrypterFactory to allow AES256 encryption algorithm
    Pair: @IreneLau-GDS, @hugh-emerson

--- src/main/java/uk/gov/ida/saml/security/EncrypterFactory.java
+++ src/main/java/uk/gov/ida/saml/security/EncrypterFactory.java
@@ -9,8 +9,20 @@ import org.opensaml.xmlsec.encryption.support.KeyEncryptionParameters;
 public class EncrypterFactory {
 
     public Encrypter createEncrypter(Credential credential) {
+        return createAES128Encrypter(credential);
+    }
+
+    public Encrypter createAES128Encrypter(Credential credential) {
+        return createEncrypter(credential, EncryptionConstants.ALGO_ID_BLOCKCIPHER_AES128);
+    }
+
+    public Encrypter createAES256Encrypter(Credential credential) {
+        return createEncrypter(credential, EncryptionConstants.ALGO_ID_BLOCKCIPHER_AES256);
+    }
+
+    private Encrypter createEncrypter(Credential credential, String dataEncryptionAlgorithm) {
         DataEncryptionParameters encParams = new DataEncryptionParameters();
-        encParams.setAlgorithm(EncryptionConstants.ALGO_ID_BLOCKCIPHER_AES128);
+        encParams.setAlgorithm(dataEncryptionAlgorithm);
 
         KeyEncryptionParameters kekParams = new KeyEncryptionParameters();
         kekParams.setEncryptionCredential(credential);
