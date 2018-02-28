0b4b1ee6fab88641f18efead2a71516895facaa9
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2018-02-28 11:14:42 +0000

    refactor: use OpenSAMLs libraries for serialization
    Rather using our own libraries for serializing SAML java objects we can
    use the libraries that OpenSAML provides:
    - `XMLObjectSupport` is used to convert objects into Elements
    - `SerializeSupport` is used to convert Elements into Strings
    - `Base64Support` is used to base64 encode Strings
    
    These changes should be backwards compatible.

--- saml-serializers/src/main/java/uk/gov/ida/saml/serializers/XmlObjectToBase64EncodedStringTransformer.java
+++ saml-serializers/src/main/java/uk/gov/ida/saml/serializers/XmlObjectToBase64EncodedStringTransformer.java
@@ -1,49 +1,24 @@
 package uk.gov.ida.saml.serializers;
 
-import com.google.common.base.Throwables;
+import net.shibboleth.utilities.java.support.codec.Base64Support;
+import net.shibboleth.utilities.java.support.xml.SerializeSupport;
+import org.apache.commons.codec.binary.StringUtils;
 import org.opensaml.core.xml.XMLObject;
-import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
-import org.opensaml.core.xml.io.Marshaller;
-import org.opensaml.core.xml.io.MarshallerFactory;
-import org.opensaml.core.xml.io.MarshallingException;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import uk.gov.ida.shared.utils.string.StringEncoding;
-import uk.gov.ida.shared.utils.xml.XmlUtils;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.util.function.Function;
 
 public class XmlObjectToBase64EncodedStringTransformer<TInput extends XMLObject> implements Function<TInput,String> {
 
     @Override
     public String apply(XMLObject signableXMLObject) {
-        String result;
         Element signedElement = marshallToElement(signableXMLObject);
-        result = XmlUtils.writeToString(signedElement);
-        return StringEncoding.toBase64Encoded(result);
+        String node = SerializeSupport.nodeToString(signedElement);
+        return Base64Support.encode(StringUtils.getBytesUtf8(node), Base64Support.UNCHUNKED);
     }
 
     private static Element marshallToElement(XMLObject rootObject) {
-        Element result;
-        try {
-            marshallToXml(rootObject);
-            result = rootObject.getDOM();
-        } catch (ParserConfigurationException | MarshallingException e) {
-            throw Throwables.propagate(e);
-        }
-        return result;
-    }
-
-    private static Document marshallToXml(XMLObject samlXml) throws ParserConfigurationException, MarshallingException {
-        MarshallerFactory marshallerFactory = XMLObjectProviderRegistrySupport.getMarshallerFactory();
-
-        Marshaller responseMarshaller = marshallerFactory.getMarshaller(samlXml);
-
-        Document document = XmlUtils.newDocumentBuilder().newDocument();
-        responseMarshaller.marshall(samlXml, document);
-
-        return document;
+        return new XmlObjectToElementTransformer<>().apply(rootObject);
     }
 
 }
--- saml-serializers/src/main/java/uk/gov/ida/saml/serializers/XmlObjectToElementTransformer.java
+++ saml-serializers/src/main/java/uk/gov/ida/saml/serializers/XmlObjectToElementTransformer.java
@@ -1,42 +1,20 @@
 package uk.gov.ida.saml.serializers;
 
-import com.google.common.base.Throwables;
 import org.opensaml.core.xml.XMLObject;
-import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
-import org.opensaml.core.xml.io.Marshaller;
-import org.opensaml.core.xml.io.MarshallerFactory;
 import org.opensaml.core.xml.io.MarshallingException;
-import org.w3c.dom.Document;
+import org.opensaml.core.xml.util.XMLObjectSupport;
 import org.w3c.dom.Element;
-
-import javax.xml.parsers.ParserConfigurationException;
 
 import java.util.function.Function;
 
-import static uk.gov.ida.shared.utils.xml.XmlUtils.newDocumentBuilder;
 
 public class XmlObjectToElementTransformer<TInput extends XMLObject> implements Function<TInput,Element> {
 
     public Element apply(TInput rootObject) {
-        Element result;
         try {
-            marshallToXml(rootObject);
-            result = rootObject.getDOM();
-        } catch (ParserConfigurationException | MarshallingException e) {
-            throw Throwables.propagate(e);
+            return XMLObjectSupport.marshall(rootObject);
+        } catch (MarshallingException e) {
+            throw new RuntimeException(e);
         }
-        return result;
     }
-
-    private Document marshallToXml(TInput samlXml) throws ParserConfigurationException, MarshallingException {
-        MarshallerFactory marshallerFactory = XMLObjectProviderRegistrySupport.getMarshallerFactory();
-
-        Marshaller responseMarshaller = marshallerFactory.getMarshaller(samlXml);
-
-        Document document = newDocumentBuilder().newDocument();
-        responseMarshaller.marshall(samlXml, document);
-
-        return document;
-    }
-
 }
