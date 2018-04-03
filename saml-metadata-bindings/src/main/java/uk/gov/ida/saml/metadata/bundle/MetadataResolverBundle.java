9f0eb659019b7cbcde9e72f063068a2954149148
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2018-04-03 13:55:47 +0100

    firebreak: API for metadata SignatureTrustEngine
    Wherever we validate signatures using opensaml libraries we need to
    create a SignatureTrustEngine object that does validation and credential
    fetching. When using creds from Metadata we have so far duplicated the
    building of these objects across our codebase. This change introduces
    a single way of doing this that can be used everywhere, which should
    make our code easier to understand and support.

--- saml-metadata-bindings/src/main/java/uk/gov/ida/saml/metadata/bundle/MetadataResolverBundle.java
+++ saml-metadata-bindings/src/main/java/uk/gov/ida/saml/metadata/bundle/MetadataResolverBundle.java
@@ -4,9 +4,13 @@ import com.google.inject.Module;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
 import org.opensaml.saml.metadata.resolver.MetadataResolver;
+import org.opensaml.xmlsec.signature.support.SignatureTrustEngine;
+import org.opensaml.xmlsec.signature.support.impl.ExplicitKeySignatureTrustEngine;
 import uk.gov.ida.saml.metadata.MetadataResolverConfiguration;
 import uk.gov.ida.saml.metadata.factories.DropwizardMetadataResolverFactory;
+import uk.gov.ida.saml.metadata.factories.MetadataSignatureTrustEngineFactory;
 
 import javax.inject.Provider;
 
@@ -14,6 +18,7 @@ public class MetadataResolverBundle<T extends Configuration> implements io.dropw
     private MetadataConfigurationExtractor<T> configExtractor;
     private MetadataResolver metadataResolver;
     private DropwizardMetadataResolverFactory dropwizardMetadataResolverFactory = new DropwizardMetadataResolverFactory();
+    private ExplicitKeySignatureTrustEngine signatureTrustEngine;
 
     public MetadataResolverBundle(MetadataConfigurationExtractor<T> configExtractor) {
         this.configExtractor = configExtractor;
@@ -23,6 +28,7 @@ public class MetadataResolverBundle<T extends Configuration> implements io.dropw
     public void run(T configuration, Environment environment) throws Exception {
         MetadataResolverConfiguration metadataConfiguration = configExtractor.getMetadataConfiguration(configuration);
         metadataResolver = dropwizardMetadataResolverFactory.createMetadataResolver(environment, metadataConfiguration);
+        signatureTrustEngine = new MetadataSignatureTrustEngineFactory().createSignatureTrustEngine(metadataResolver);
     }
 
     @Override
@@ -36,6 +42,15 @@ public class MetadataResolverBundle<T extends Configuration> implements io.dropw
 
     public Provider<MetadataResolver> getMetadataResolverProvider() {
         return () -> metadataResolver;
+    }
+
+    @Nullable
+    public ExplicitKeySignatureTrustEngine getSignatureTrustEngine() {
+        return signatureTrustEngine;
+    }
+
+    public Provider<ExplicitKeySignatureTrustEngine> getSignatureTrustEngineProvider() {
+        return () -> signatureTrustEngine;
     }
 
     public Module getMetadataModule() {
