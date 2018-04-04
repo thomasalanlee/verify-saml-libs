6dacb99269e699b85779dbf0798cd1043e85d0b8
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2018-04-04 10:53:49 +0100

    Replace InputStream with OpenSamlXMLObjectUnmarshaller
    The InputStream stuff is potentially causing issues running tests
    within a Docker container (for CI).
    
    Authors: @vixus0 @jhjava

--- saml-metadata-bindings/src/test/java/uk/gov/ida/saml/metadata/ExpiredCertificateMetadataFilterTest.java
+++ saml-metadata-bindings/src/test/java/uk/gov/ida/saml/metadata/ExpiredCertificateMetadataFilterTest.java
@@ -1,44 +1,41 @@
 package uk.gov.ida.saml.metadata;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.fail;
-
-import java.io.InputStream;
-
-import org.apache.commons.io.IOUtils;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.opensaml.core.config.InitializationService;
 import org.opensaml.core.xml.XMLObject;
-import org.opensaml.core.xml.util.XMLObjectSupport;
 import org.opensaml.saml.metadata.resolver.filter.FilterException;
 import org.opensaml.saml.metadata.resolver.filter.MetadataFilter;
-
-import net.shibboleth.utilities.java.support.xml.BasicParserPool;
 import uk.gov.ida.saml.core.test.TestCertificateStrings;
+import uk.gov.ida.saml.deserializers.OpenSamlXMLObjectUnmarshaller;
+import uk.gov.ida.saml.deserializers.parser.SamlObjectParser;
 import uk.gov.ida.saml.metadata.test.factories.metadata.MetadataFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 public class ExpiredCertificateMetadataFilterTest {
 
     private MetadataFactory metadataFactory = new MetadataFactory();
     private MetadataFilter metadataFilter;
-    private BasicParserPool parserPool = new BasicParserPool();
+    private OpenSamlXMLObjectUnmarshaller<XMLObject> unmarshaller = new OpenSamlXMLObjectUnmarshaller<>(new SamlObjectParser());
 
     @Before
     public void setUp() throws Exception {
         metadataFilter = new ExpiredCertificateMetadataFilter();
-        parserPool.initialize();
+        InitializationService.initialize();
     }
 
     @Test
-    public void shouldFailToFilterLoadingValidMetadataWhenSignedWithExpiredCertificate() throws Exception {
+    public void shouldFailToFilterLoadingValidMetadataWhenSignedWithExpiredCertificate() {
         try {
             DateTimeUtils.setCurrentMillisFixed(DateTime.now().plusYears(1000).getMillis());
-            InputStream inputStream = IOUtils.toInputStream(metadataFactory.signedMetadata(TestCertificateStrings.METADATA_SIGNING_A_PUBLIC_CERT, TestCertificateStrings.METADATA_SIGNING_A_PRIVATE_KEY));
-            XMLObject metadata = XMLObjectSupport.unmarshallFromInputStream(parserPool, inputStream);
+            String signedMetadata = metadataFactory.signedMetadata(TestCertificateStrings.METADATA_SIGNING_A_PUBLIC_CERT, TestCertificateStrings.METADATA_SIGNING_A_PRIVATE_KEY);
+            XMLObject metadata = unmarshaller.fromString(signedMetadata);
             metadataFilter.filter(metadata);
             fail("Expected exception not thrown");
         } catch (FilterException e){
@@ -49,11 +46,11 @@ public class ExpiredCertificateMetadataFilterTest {
     }
 
     @Test
-    public void shouldFailToFilterLoadingValidMetadataWhenSignedWithNotYetValidCertificate() throws Exception {
+    public void shouldFailToFilterLoadingValidMetadataWhenSignedWithNotYetValidCertificate() {
         try {
             DateTimeUtils.setCurrentMillisFixed(DateTime.now().minusYears(1000).getMillis());
-            InputStream inputStream = IOUtils.toInputStream(metadataFactory.signedMetadata(TestCertificateStrings.METADATA_SIGNING_A_PUBLIC_CERT, TestCertificateStrings.METADATA_SIGNING_A_PRIVATE_KEY));
-            XMLObject metadata = XMLObjectSupport.unmarshallFromInputStream(parserPool, inputStream);
+            String signedMetadata = metadataFactory.signedMetadata(TestCertificateStrings.METADATA_SIGNING_A_PUBLIC_CERT, TestCertificateStrings.METADATA_SIGNING_A_PRIVATE_KEY);
+            XMLObject metadata = unmarshaller.fromString(signedMetadata);
             metadataFilter.filter(metadata);
             fail("Expected exception not thrown");
         } catch (FilterException e){
@@ -65,8 +62,8 @@ public class ExpiredCertificateMetadataFilterTest {
 
     @Test
     public void shouldFilterMetadataSuccessfully() throws Exception {
-        InputStream inputStream = IOUtils.toInputStream(metadataFactory.signedMetadata(TestCertificateStrings.METADATA_SIGNING_A_PUBLIC_CERT, TestCertificateStrings.METADATA_SIGNING_A_PRIVATE_KEY));
-        XMLObject metadata = XMLObjectSupport.unmarshallFromInputStream(parserPool, inputStream);
+        String signedMetadata = metadataFactory.signedMetadata(TestCertificateStrings.METADATA_SIGNING_A_PUBLIC_CERT, TestCertificateStrings.METADATA_SIGNING_A_PRIVATE_KEY);
+        XMLObject metadata = unmarshaller.fromString(signedMetadata);
         metadata = metadataFilter.filter(metadata);
         Assert.assertNotNull("metadata should not have been filtered out", metadata);
     }
