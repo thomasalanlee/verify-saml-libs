1d43c64c9b080572158d867410d2df6460cf0e25
James Howes <james.howes@digital.cabinet-office.gov.uk>
2018-04-12 16:18:09 +0100

    EID-424 - Update Condition builder with new builder methods
    Co-authored-by: Simon Worthington <simon.worthington@digital.cabinet-office.gov.uk>
    Co-authored-by: Hoque Ali <hoque.ali@digital.cabinet-office.gov.uk>

--- saml-utils/src/main/java/uk/gov/ida/saml/core/test/builders/ConditionsBuilder.java
+++ saml-utils/src/main/java/uk/gov/ida/saml/core/test/builders/ConditionsBuilder.java
@@ -1,8 +1,11 @@
 package uk.gov.ida.saml.core.test.builders;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.opensaml.core.xml.XMLObjectBuilderFactory;
+import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
 import org.opensaml.saml.saml2.core.AudienceRestriction;
 import org.opensaml.saml.saml2.core.Conditions;
-import uk.gov.ida.saml.core.test.OpenSamlXmlObjectFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,22 +14,28 @@ import static uk.gov.ida.saml.core.test.builders.AudienceRestrictionBuilder.anAu
 
 public class ConditionsBuilder {
 
-    private OpenSamlXmlObjectFactory openSamlXmlObjectFactory = new OpenSamlXmlObjectFactory();
+    private XMLObjectBuilderFactory xmlObjectBuilderFactory = XMLObjectProviderRegistrySupport.getBuilderFactory();
     private List<AudienceRestriction> audienceRestrictions = new ArrayList<>();
     private AudienceRestriction defaultAudienceRestriction = anAudienceRestriction().build();
     private boolean shouldIncludeDefaultAudienceRestriction = true;
+    private DateTime notBefore = null;
+    private DateTime notOnOrAfter = null;
 
     public static ConditionsBuilder aConditions() {
         return new ConditionsBuilder();
     }
 
     public Conditions build() {
-        Conditions conditions = openSamlXmlObjectFactory.createConditions();
+        Conditions conditions = (Conditions)xmlObjectBuilderFactory
+                .getBuilder(Conditions.DEFAULT_ELEMENT_NAME)
+                .buildObject(Conditions.DEFAULT_ELEMENT_NAME, Conditions.TYPE_NAME);
 
         if (shouldIncludeDefaultAudienceRestriction) {
             audienceRestrictions.add(defaultAudienceRestriction);
         }
         conditions.getAudienceRestrictions().addAll(audienceRestrictions);
+        conditions.setNotBefore(notBefore);
+        conditions.setNotOnOrAfter(notOnOrAfter);
 
         return conditions;
     }
@@ -38,6 +47,27 @@ public class ConditionsBuilder {
 
     public ConditionsBuilder addAudienceRestriction(AudienceRestriction audienceRestriction) {
         shouldIncludeDefaultAudienceRestriction = false;
+        audienceRestrictions.add(audienceRestriction);
+        return this;
+    }
+
+    public ConditionsBuilder withNotBefore(DateTime notBefore){
+        this.notBefore = notBefore;
+        return this;
+    }
+
+    public ConditionsBuilder withNotOnOrAfter(DateTime notOnOrAfter){
+        this.notOnOrAfter = notOnOrAfter;
+        return this;
+    }
+
+    public ConditionsBuilder validFor(Duration duration){
+        withNotBefore(DateTime.now()).withNotOnOrAfter(DateTime.now().plus(duration));
+        return this;
+    }
+
+    public ConditionsBuilder restrictedToAudience(String audienceUri){
+        AudienceRestriction audienceRestriction = anAudienceRestriction().withAudienceId(audienceUri).build();
         audienceRestrictions.add(audienceRestriction);
         return this;
     }
